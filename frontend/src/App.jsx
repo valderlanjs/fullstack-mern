68bc7b0bfdc12641d246823831bf53a8851f9679
Valderlan Silva <valderlanjosr15@gmail.com>
2024-12-17 15:20:36 -0300

    finalizado a criação dos arquivos para criação do frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 
 import Header from "./components/Header";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Collection from "./pages/Collection";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Verify from "./pages/Verify";
 
 export default function App() {
   return (
@@ -10,7 +20,15 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/verify" element={<Verify />} />
       </Routes>
     </div>
   )
